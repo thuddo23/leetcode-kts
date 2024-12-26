4f5a73f250c0b8d3a8fb1722c96184969f11b3aa
teddydo <thuandd@gmail.com>
2024-12-26 15:28:13 +0700

    53 - maximum subarray

--- src/main/kotlin/array/53-maximum_subarray.kt
+++ src/main/kotlin/array/53-maximum_subarray.kt
@@ -1,25 +1,20 @@
 package org.example.array
 
 fun maxSubArray(nums: IntArray): Int {
-    var start = 0
-    var end = 0
     var currentSum = Int.MIN_VALUE
     var max = Int.MIN_VALUE
-    for ((index, num) in nums.withIndex()) {
+    for (num in nums) {
         if (currentSum < 0) {
             currentSum = num
             if (num > max) {
                 max = num
             }
-            start = index
-            end = index
             continue
         }
         currentSum += num
         if (max < currentSum) {
-            end = index
             max = currentSum
         }
     }
-    return kotlin.math.max(max, nums.first())
+    return max
 }
