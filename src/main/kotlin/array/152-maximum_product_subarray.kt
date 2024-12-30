9a6dad646c3e1950e0031080e5be7714814fb338
teddydo <thuandd@gmail.com>
2024-12-30 11:54:45 +0700

    152: maximum product subarray: complete first version with O(n) complexity

--- src/main/kotlin/array/152-maximum_product_subarray.kt
+++ src/main/kotlin/array/152-maximum_product_subarray.kt
@@ -6,16 +6,35 @@ fun maxProduct(nums: IntArray): Int {
     var firstNev: Int? = null
     var lastNev: Int? = null
     var current = 1
+    var count = 0
     var max = Int.MIN_VALUE
+
+    var getMax = {
+        if (current < 0 && firstNev != null && count > 1) {
+            if (lastNev != null) {
+                current = kotlin.math.max(current / firstNev!!, current / lastNev!!)
+            } else {
+                current/=firstNev!!
+            }
+        }
+        if (count == 0) {
+            max
+        } else {
+            kotlin.math.max(current, max)
+        }
+    }
     for (num in nums) {
         if (num == 0) {
+            max = max.coerceAtLeast(0)
+            max = getMax()
             firstNev = null
             lastNev = null
             current = 1
-            max = max.coerceAtLeast(0)
+            count = 0
             continue
         }
         current *= num
+        count++
         max = kotlin.math.max(max, current)
         if (num > 0 && lastNev != null) {
             lastNev *= num
@@ -28,16 +47,9 @@ fun maxProduct(nums: IntArray): Int {
             }
         }
     }
-    if (current < 0 && firstNev != null) {
-        if (lastNev != null) {
-            current = kotlin.math.max(current / firstNev, current / lastNev)
-        } else {
-            current/=firstNev
-        }
-    }
-    return kotlin.math.max(current, max)
+    return getMax()
 }
 
 fun main() {
-    print(maxProduct(intArrayOf(-2, 0, -1)))
+    print(maxProduct(intArrayOf(-1, -2, -3, 0)))
 }
